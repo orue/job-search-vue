25012e94b819c1f6e0aa1c9ea038ab15c97fa281
Carlos Orue <carlos@orue.io>
2022-04-19 16:18:00 -0500

    add the Main Navigation bar

--- src/playground.js
+++ src/playground.js
@@ -3,3 +3,23 @@ const fruits = ["Apple", "Banana", "Orange"];
 const vegetables = ["Cucumber", "Radish"];
 
 console.log([...fruits, ...vegetables]);
+
+// Javascript Objects, Properties and Methods
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ["Vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+
+developer.doubleSalary();
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
