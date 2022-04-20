41e46d753039ce4982ce82ec2747212c63e89853
Carlos Orue <carlos@orue.io>
2022-04-20 09:15:01 -0500

    add unit test playground

--- src/playground.js
+++ src/playground.js
@@ -1,25 +1,7 @@
-// ES6 Array Spreading
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
-
-console.log([...fruits, ...vegetables]);
-
-// Javascript Objects, Properties and Methods
-const developer = {
-  salary: 100000,
-  experience: 4.5,
-  techStack: ["Vue", "HTML", "CSS"],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
-  },
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
 };
-
-console.log(developer.salary);
-console.log(developer.lookingForWork);
-
-developer.doubleSalary();
-
-console.log(developer.salary);
-console.log(developer.lookingForWork);
