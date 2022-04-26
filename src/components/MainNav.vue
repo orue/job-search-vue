41f4d6cb5bec9bca07d246721ce13c6da4a5e4a4
Carlos Orue <carlos@orue.io>
2022-04-26 18:26:44 -0500

    add main-nav and test

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -13,6 +13,20 @@
         <a :href="url" class="flex items-center h-full text-xl">{{
           company
         }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+              data-test="main-test-list-item"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -25,6 +39,14 @@ export default {
     return {
       company: "Elocin Careers",
       url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Elocin",
+        "How we hire",
+        "Studens",
+        "Jobs",
+      ],
     };
   },
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,20 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Elocin Careers");
   });
+
+  it("display menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-test-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Elocin",
+      "How we hire",
+      "Studens",
+      "Jobs",
+    ]);
+  });
 });
